db0088da1c0fe907c8f409c2d9934b5b5ef15f67
cgl <928487318@qq.com>
2019-02-27 14:36:34 +0800

    修改路径问题

--- src/pages/joinIn/getJoin.js
+++ src/pages/joinIn/getJoin.js
@@ -2,11 +2,12 @@
 import styles from './index.css'
 
 const doimg = require('../../assets/img/join-do.png');
+const merchantImg = require('../../assets/img/join-Merchant.jpg');
 const doUrl = '/joinIn';
 export default function() {
     return (
         <div className={styles.imgBanner}>
-            <img className={styles.img} src={require('../../assets/img/join-Merchant.jpg')} />
+            <img className={styles.img} src={merchantImg} />
             <div className={styles.imgBackground}>
                 <a href={doUrl}>
                     <img className={styles.img} src={doimg} />
