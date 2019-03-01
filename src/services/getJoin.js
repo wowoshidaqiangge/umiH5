19813a62861951f6deb9dbcc223e0107b0a1c29a
cgl <928487318@qq.com>
2019-03-01 20:30:49 +0800

    1

--- src/services/getJoin.js
+++ src/services/getJoin.js
@@ -4,6 +4,7 @@ import sys from '../utils/request'
 const sysParams = sys.sysParams
 
 export async function getJoinIn() {
-  return axios.post(sys.api.join.getJoinIn, sysParams)
+  // return axios.post(sys.api.join.getJoinIn, sysParams)
+  return axios.post(sys.api.join.getJoinIn)
 }
 
