0d80ea57e9f20664a044630632d8ab0464201299
cgl <928487318@qq.com>
2019-03-05 17:28:14 +0800

    处理低版本问题

--- src/pages/joinIn/getJoin.js
+++ src/pages/joinIn/getJoin.js
@@ -20,12 +20,12 @@ class GetJoin extends Component {
   //捕获异常
   componentDidCatch(error, info) {
     console.log('错误信息~~~~',error, info);
-    alert('捕获到异常'+error+'==='+JSON.stringify(info));
-
-    window.onerror = function(errorMessage, scriptURI, lineNumber,columnNumber,errorObj) {
-      // code..
-      alert('this is error'+errorMessage+'=='+scriptURI+'=='+lineNumber+'=='+columnNumber+'=='+errorObj);
-    }
+    // alert('捕获到异常'+error+'==='+JSON.stringify(info));
+    //
+    // window.onerror = function(errorMessage, scriptURI, lineNumber,columnNumber,errorObj) {
+    //   // code..
+    //   alert('this is error'+errorMessage+'=='+scriptURI+'=='+lineNumber+'=='+columnNumber+'=='+errorObj);
+    // }
   }
 
   handleNextUrl() {
